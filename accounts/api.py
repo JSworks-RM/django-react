c248e54a726bc835827e6cbdae5e4030bfbea6ea
Nacho <joseignaciorm13@gmail.com>
2020-04-15 11:23:09 +0200

    Token Authentication Part 4: Created LoginAPI in the Backend

--- accounts/api.py
+++ accounts/api.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 # Register API
 class RegisterAPI(generics.GenericAPIView):
@@ -13,10 +13,22 @@ class RegisterAPI(generics.GenericAPIView):
         user = serializer.save()
         token = AuthToken.objects.create(user)[1]
         return Response({
-            'user': UserSerializer(user, context=self.get_serializer_context()).data,
-             'token': token
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": token
         })
 
 # Login API
+class LoginAPI(generics.GenericAPIView):
+  serializer_class = LoginSerializer
+
+  def post(self, request, *args, **kwargs):
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data
+    token = AuthToken.objects.create(user)[1]
+    return Response({
+      "user": UserSerializer(user, context=self.get_serializer_context()).data,
+      "token": token
+    })
 
 # Get User API
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,18 +10,28 @@ class UserSerializer(serializers.ModelSerializer):
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+  class Meta:
+      model = User
+      fields = ('id', 'username', 'email', 'password')
+      extra_kwargs = {'password': {'write_only': True}}
+  def create(self, validated_data):
+      user = User.objects.create_user(
+          validated_data['username'],
+          validated_data['email'],
+          validated_data['password']
+      )
+      return user
+      
+# Login Serializer: 
+# We do not deal with models serializers becouse we do not create nothing, 
+# just validated a user who is auhenticate it
+class LoginSerializer(serializers.Serializer):
+  username = serializers.CharField()
+  password = serializers.CharField()
 
-        def create(self, validated_data):
-            user = User.objects.create(
-                validated_data['username'],
-                validated_data['email'],
-                validated_data['password']
-            )
-
-            return user
-
-# Login Serializer
+  def validate(self, data):
+    user = authenticate(**data)
+    if user and user.is_active:
+      return user
+    raise serializers.ValidationError("Incorrect Credentials")
+        
