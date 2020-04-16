d64c3815b90bdb7f653e132d371cbae6277091d9
Nacho <joseignaciorm13@gmail.com>
2020-04-16 13:01:28 +0200

    Frontend Auth part 1: Added login functionality

--- frontend/src/components/accounts/Login.js
+++ frontend/src/components/accounts/Login.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { login } from '../../redux/actions/actionAuth'
+
 
 class Login extends Component {
+
     state = {
         username: "",
         password: ""
+    }
+
+    static propTypes = {
+        login: PropTypes.func.isRequired,
+        isAuthenticated: PropTypes.bool
     }
 
     onSubmit = e => {
         e.preventDefault()
         // Here we will want to call a login action inside of an auth 
         // in an auth.js file in the action folder
-        console.log('Submit')
+        this.props.login(this.state.username, this.state.password)
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
 
     render () {
+        if(this.props.isAuthenticated) {
+            return <Redirect to="/" />
+        }
+
         const { username, password } = this.state
         return (
             <div className="col-md-6 m-auto">
@@ -58,4 +72,7 @@ class Login extends Component {
     }
 }
 
-export default Login
+const mapStateToProps = state => ({
+    isAuthenticated: state.authReducer.isAuthenticated
+})
+export default connect(mapStateToProps, { login })(Login)
--- frontend/src/redux/actions/actionAuth.js
+++ frontend/src/redux/actions/actionAuth.js
@@ -4,7 +4,9 @@ import { returnErrors } from './actionMessages'
 import {
     USER_LOADING,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from './actionTypes'
 
 
@@ -50,4 +52,37 @@ export const loadUser = () => (dispatch, getState) =>{
 
 // All we have done is to create loadUser and we are not actually calling it now.
 // We want to get or load all this loadUser pretty much all the time so we go to bring this action in the main app
-// In order to call it there it will be after DOM have loaed using componentDidMount()
+// In order to call it there it will be after DOM have loaded using componentDidMount()
+
+
+// ********************* LOGIN FUNTION *********************
+// We need to get username and password in order to validate it againts the backend
+export const login = (username, password) => (dispatch) =>{
+
+    // Header payload 
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    // Request body. Turn it out into a json 
+const body = JSON.stringify({ username, password })
+
+    // Post request to login
+    axios.post('/api/auth/login', body, config)
+        .then(res => {
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            // Dispatch returnError just to put errors into the state. 
+            // we alse gonna call AUTH_ERROR to fire that action if we are not login to set defaults state 
+            dispatch (returnErrors(err.response.data, err.response.status))
+            dispatch ({
+                type: LOGIN_FAIL
+            })
+        })
+}
